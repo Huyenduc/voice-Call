0234d371aab1d7bdd88cb1a35360a43e2bed8647
Duc <Huyenduc0601@gmail.com>
2022-11-17 13:54:30 +0700

    pushcode

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { Text, View } from 'react-native'
+import Test  from './src/test'
+
+const App = () => {
+  // const [open, setOpen] = useState(true);
+  return (
+    <Test/>
+    // <View>
+    //   <Text>dslmdmscsc sc s,ddjqbdqjdqjdjqnjdn</Text>
+    // </View>
+  )
+}
+
+export default App
